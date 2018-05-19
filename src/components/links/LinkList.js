78d6117e73b2ea335a57b58c183a000a40224d2b
shreeramneupane <shreeramneupane0@gmail.com>
2018-05-19 13:29:39 +0545

    List link through api

--- src/components/links/LinkList.js
+++ src/components/links/LinkList.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
 
+// Material Component
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
+
 // Component
 import Header from '../Header';
 
+// Service
+import RebrandlyApi from '../../services/rebrandlyApi';
+
 class LinkList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      links: []
+    }
+  }
   render() {
     return (
       <div>
         <Header />
-        This is list page
+        <Table selectable={false}>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+              <TableHeaderColumn>#</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Destination</TableHeaderColumn>
+              <TableHeaderColumn>Short URL</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+          {
+          this.state.links.map((link, index) => {
+            return(
+              <TableRow key={link.id} selectable={false}>
+                <TableRowColumn>{index}</TableRowColumn>
+                <TableRowColumn>{link.title}</TableRowColumn>
+                <TableRowColumn>{link.destination}</TableRowColumn>
+                <TableRowColumn>{link.shortUrl}</TableRowColumn>
+              </TableRow>
+            )
+          })
+        }
+          </TableBody>
+        </Table>
       </div>
     )
+  }
+
+  componentWillMount() {
+    RebrandlyApi.get('/links')
+    .then(links => {
+      this.setState({
+        links: links
+      })
+    })
   }
 }
 
